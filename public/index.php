7fc73c30935ced08606252e92c414282b78e40d0
winston <andyredjohn@gmail.com>
2024-12-11 11:36:19 -0600

    update public index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
+<?php
+// 定义项目根目录常量
+define('ROOT_PATH', dirname(__DIR__));
+
+// 引入必要的类和配置
+require_once ROOT_PATH . '/src/Controller/Home/HomeController.php';
+require_once ROOT_PATH . '/config/config.php';
+
+// 如果有请求访问CMS
+if (isset($_GET['access_cms'])) {
+    // 实例化控制器
+    $controller = new \Controller\Home\HomeController();
+    // 调用index方法
+    $controller->index();
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +31,8 @@
     <!-- 修改背景图片路径，使用相对于当前目录的路径 -->
     <img id="bgImage" class="bg-image" src="./images/background.jpeg" alt="Background">
     
-    <!-- CMS入口链接 -->
-    <a href="./views/home.php" id="cmsEnter" class="cms-enter">ACCESS THE CMS</a>
+    <!-- 修改链接，添加access_cms参数 -->
+    <a href="?access_cms=1" id="cmsEnter" class="cms-enter">ACCESS THE CMS</a>
 
     <!-- 修改JavaScript文件路径，使用相对于当前目录的路径 -->
     <script src="./js/main.js"></script>
