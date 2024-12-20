f74fea5cef1620cc9d28c5994b77d401acf2f844
winston <andyredjohn@gmail.com>
2024-12-20 15:50:41 -0600

    Remove vendor/gumlet/php-image-resize from index

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'winston/photography-cms-refactor1.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a19c2efe01d7a854536aebd82548e698cd15ee1c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gumlet/php-image-resize' => array(
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => '5ef461c53241933a3e14e4582a9d2884b8261213',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gumlet/php-image-resize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'winston/photography-cms-refactor1.0' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a19c2efe01d7a854536aebd82548e698cd15ee1c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
